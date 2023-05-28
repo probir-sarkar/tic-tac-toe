3e45b7f119f948b14e92ad57899907da30b0ff55
Probir Sarkar <probirsarkar855@gmail.com>
2023-05-28 18:19:07 +0530

    move to vite from create react app

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.scss";
+import App from "./App.jsx";
 import PlayersProvider from "./contexts/players.context";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <PlayersProvider>
     <React.StrictMode>
