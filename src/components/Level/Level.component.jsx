6985a41b7b2a8d71380b948c728f5c607168e36f
Probir Sarkar <probirsarkar855@gmail.com>
2025-02-02 21:53:32 +0530

    more optimize

--- src/components/Level/Level.component.jsx
+++ src/components/Level/Level.component.jsx
@@ -22,10 +22,12 @@ const Level = () => {
   return (
     <div className="flex flex-grow justify-center items-center mb-10 my-4 gap-5  flex-wrap">
       {levels.map(({ id, label }) => (
-        <button
+        <motion.button
           key={id}
-          className={`relative  px-10 py-2.5 border-2 dark:border-white border-black rounded  flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out `}
+          className={`relative  px-10 py-2.5 border-2 dark:border-white bg-white border-black dark:bg-black rounded  flex justify-center items-center cursor-pointer transition-all duration-300 ease-in-out `}
           onClick={() => handleLevel(id)}
+          whileHover={{ scale: 1.05 }} // Hover animation
+          whileTap={{ scale: 0.95 }} // Tap animation
         >
           <span className="mix-blend-difference z-10 text-white">{label}</span>
           <AnimatePresence>
@@ -33,11 +35,14 @@ const Level = () => {
               <motion.div
                 layout="position"
                 layoutId="underline"
+                initial={{ opacity: 0 }} // Initial animation state
+                animate={{ opacity: 1 }} // Animate when active
+                exit={{ opacity: 0 }} // Exit animation
                 className="absolute rounded-xs top-0 left-0 w-full h-full dark:bg-white bg-black "
               />
             )}
           </AnimatePresence>
-        </button>
+        </motion.button>
       ))}
     </div>
   );
